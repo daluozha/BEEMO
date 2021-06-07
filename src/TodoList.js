5c5a5017d8da16a1eb39532cd48288940a285e0e
daluozha <561890199@qq.com>
2021-06-07 17:44:25 +0800

    add css transition

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,18 +2,26 @@ import React, { Component, Fragment } from "react";
 import './style.css'
 import TodoItem from "./TodoItem";
 import axios from "axios";
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             inputValue: '',
-            list: ['你好','nihao','halo','hello','ohayo']
+            list: ['你好','nihao','halo','hello','ohayo'],
+            show: true
         }
         this.handleInputChange = this.handleInputChange.bind(this)
         this.handleButtonClick = this.handleButtonClick.bind(this)
         this.handleItemDelete = this.handleItemDelete.bind(this)
+        this.handleToggle = this.handleToggle.bind(this)
 
+    }
+    handleToggle(){
+        this.setState({
+            show: !this.state.show
+        })
     }
     handleInputChange({target: { value: inputValue }}){
         this.setState(() => ({ inputValue }))
@@ -35,12 +43,21 @@ class TodoList extends Component {
         return (
             this.state.list.map((item, index) => {
                 return (
-                    <TodoItem 
-                        content={item}
-                        key={index}
-                        index={index}
-                        handleItemDelete={this.handleItemDelete}
-                    />
+                    <CSSTransition
+                        timeout={1000}
+                        classNames='fade'
+                        unmountOnExit
+                        onEntered={(el) => {el.style.color = 'blue'}}
+                        appear={true}
+                    >
+                        <TodoItem 
+                            content={item}
+                            key={index}
+                            index={index}
+                            handleItemDelete={this.handleItemDelete}
+                        />
+                    </CSSTransition>
+                    
                 )
             })
         )
@@ -67,10 +84,29 @@ class TodoList extends Component {
                         onChange={this.handleInputChange}
                     />
                     <button onClick={this.handleButtonClick}>submit</button>
+
                 </div>                
                 <ul>
-                    { this.getTodoItem() }
+                    <TransitionGroup
+
+                    >
+                        { this.getTodoItem() }
+                    </TransitionGroup>
                 </ul>
+
+                <div>
+                    <CSSTransition
+                        in={this.state.show}
+                        timeout={1000}
+                        classNames='fade'
+                        unmountOnExit
+                        onEntered={(el) => {el.style.color = 'blue'}}
+                        appear={true}
+                    >
+                        <div>hello</div>
+                    </CSSTransition>
+                    <button onClick={this.handleToggle}>toggle</button>
+                </div>
             </Fragment>
         );
     }
